fe48682329a4c65ac962d9fa42965b92a07f9db0
Darren <dl605117@gmail.com>
2020-02-28 13:29:47 -0800

    Add Write back signal to fetch and move register file to decode_reg
    Add Write back signal to fetch and move register file to decode_reg

--- cpu/fetch.v
+++ cpu/fetch.v
@@ -1,6 +1,8 @@
 module fetch (
     input clk_i
-  , input branch
+  , input branch_i
+  , input pc_wb_i
+  , input data_i
   , output [31:0] inst_o
 );
 
@@ -13,7 +15,7 @@ module fetch (
   assign pc_plus_8 = pc_r + 8;
   assign pc_plus_12 = pc_r + 12;
 
-  code_memory cm (  .clk(clk)
+  code_memory cm (  .clk(clk_i)
                   , .pc_i(pc_r)
                   , .inst(inst_o)
                   );
@@ -21,12 +23,12 @@ module fetch (
   initial pc_r = 0;
 
   always @(*) begin ////check PLS!!!!!
-    if ( branch )   // Does Branch with Conditions
+    if ( branch_i )   // Does Branch with Conditions
       pc_n = pc_plus_8 + { {6{branch_address[23]}}, branch_address, 2'b0 };
+    else if ( pc_wb_i ) // if writing to register 15, needs to write to PC as well
+      pc_n = data_i;
     else // Normal Increment
       pc_n = pc_plus_4;
-    else if ( rd_address == 4'b1111 ) // if writing to register 15, needs to write to PC as well
-      pc_n = data;
   end
 
   always @(posedge clk_i) pc_r <= pc_n;
